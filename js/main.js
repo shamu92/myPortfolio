793dcfbf3eafa3d35f4ea061af009fa13ba87642
shamu92 <altshteyn@gmail.com>
2016-12-21 02:10:07 -0800

    first draft done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 1) {
             $('header').css("background", "black")
-            $('header').css("opacity", ".85")
+            $('header').css("opacity", ".8")
             $('header a').css("color", "white")
 
 
@@ -11,5 +11,11 @@ $(document).ready(function() {
             $('header a').css("color", "black")
 
         }
+    });
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1250);
+        return false;
     });
 });
