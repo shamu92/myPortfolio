09a613ba86e2b0cce0c9215b0ef904106098972a
shamu92 <altshteyn@gmail.com>
2016-12-21 15:57:02 -0800

    resized images

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
             $('header').css("background", "black")
             $('header').css("opacity", ".8")
             $('header a').css("color", "white")
+            $('header a:hover').css("color", "goldenrod")
+
 
 
         } else {
             $('header').css("background", "none");
             $('header a').css("color", "black")
+            $('header a:hover').css("color", "goldenrod")
+
 
         }
     });
+    $('header a:hover').css("color", "goldenrod");
+
     $('a').click(function() {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
