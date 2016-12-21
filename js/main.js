d3a36d294e654e598d14ae0f19ba986abc94bacb
shamu92 <altshteyn@gmail.com>
2016-12-20 20:24:47 -0800

    added js and some styling

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 1) {
+            $('header').css("background", "black")
+            $('header').css("opacity", ".8")
+            $('header a').css("color", "white")
+
+
+        } else {
+            $('header').css("background", "none");
+            $('header a').css("color", "black")
+
+        }
+    });
+});
