b94a2092cd1b9b1214b88e0d453b5a8e89e508c5
shamu92 <altshteyn@gmail.com>
2016-12-21 00:04:44 -0800

    added font awesome

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 1) {
             $('header').css("background", "black")
-            $('header').css("opacity", ".8")
+            $('header').css("opacity", ".85")
             $('header a').css("color", "white")
 
 
